f3a7b8520e9b185caf4bcaed4716266fef4d0b51
ZaferBek <zaferbek22@gmail.com>
2021-10-10 21:31:18 +0100

    README file

--- src/test/java/citybike/runner/cukes_runner.java
+++ src/test/java/citybike/runner/cukes_runner.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {  "json:target/cucumber.json",
+                "html:target/default-html-reports" //this line for basic Cucumber report
+        },
         features = "src/test/resources/features/",
         glue = "citybike/step_definitions",
         dryRun = false,
